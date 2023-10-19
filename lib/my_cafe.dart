6037e2b7bbabb7803419187e136367b6b5e2fedc
KJ <rlawns0604122@gmail.com>
2023-10-19 18:28:50 +0900

    asd

--- lib/my_cafe.dart
+++ lib/my_cafe.dart
@@ -14,12 +14,37 @@ class MyCafe {
     }
   }
 
-  Future<QuerySnapshot<Map<String, dynamic>>?> get(
-      {required String collectionName}) async {
+  Future<dynamic> get({
+    required String collectionName,
+    required String? id, //고유아이디
+    required String? filedName, //이름
+    required String? filedValue,
+  }) async {
     try {
-      return db.collection(collectionName).get();
+      //전체찾기
+      if (id == null && filedName == null) {
+        return db.collection(collectionName).get();
+      } else if (id != null) {
+        //고유아이디로 찾아서 리턴
+        return db.collection(collectionName).doc(id).get();
+      } else if (filedName != null) {
+        //필드값을 가지고 찾기
+        return db
+            .collection(collectionName)
+            .where(filedName, isEqualTo: filedValue);
+      }
     } catch (e) {
       return null;
+    }
+    return null;
+  }
+
+  Future<bool> delete({required String collectionName, required id}) async {
+    try {
+      var result = await db.collection(collectionName).doc(id).delete();
+      return true;
+    } catch (e) {
+      return false;
     }
   }
 }
